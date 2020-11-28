81c2eba8357aede0e3e0e1b6de650a23f8093df2
hws522 <sraphic@kakao.com>
2020-11-28 11:37:30 +0900

    first commit

---
+++ src/main/java/com/study/test/controller/GetController.java
@@ -0,0 +1,30 @@
+package com.study.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api") // 로컬 8080/api
+public class GetController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") // localhost:8080/api/getMethod
+    public String getRequest() {
+
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter") // localhost:8080/api/getParameter?id=1234&password=abcd
+    public String getParameter(@RequestParam String id, @RequestParam String password) {
+
+        System.out.println("id : " + id);
+        System.out.println("password : " + password);
+
+        return id + password;
+    }
+
+    // localhost:8080/api/multiParameter?account=abcd&email=study@gmail.com&page=10
+
+}
---
+++ src/main/java/com/study/test/model/SearchParam.java
@@ -0,0 +1,9 @@
+package com.study.test.model;
+
+public class SearchParam {
+
+    private String account;
+    private String email;
+    private int page;
+
+}
