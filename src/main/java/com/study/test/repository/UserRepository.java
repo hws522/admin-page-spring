e3447bc2a6b873842a46779e43681965aed56f13
hws522 <sraphic@kakao.com>
2020-11-29 23:18:55 +0900

    create - repository

---
+++ src/main/java/com/study/test/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.study.test.repository;
+
+import com.study.test.model.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
