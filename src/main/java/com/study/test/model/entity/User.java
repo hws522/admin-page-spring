ae01b6b5d0e8cfc36a46e7d6b0e3e2243bf4091d
hws522 <sraphic@kakao.com>
2020-11-29 23:13:41 +0900

    create - entity

---
+++ src/main/java/com/study/test/model/entity/User.java
@@ -0,0 +1,31 @@
+package com.study.test.model.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+// @Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
