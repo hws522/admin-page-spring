aa4e6a2caba63e3b5dbb8aea3ba2116187c9a775
hws522 <sraphic@kakao.com>
2020-12-02 22:30:41 +0900

    service logic add

--- src/main/java/com/study/test/service/UserApiLogicService.java
+++ src/main/java/com/study/test/service/UserApiLogicService.java
@@ -1,6 +1,9 @@
 package com.study.test.service;
 
+import java.time.LocalDateTime;
+
 import com.study.test.ifs.CrudInterface;
+import com.study.test.model.entity.User;
 import com.study.test.model.network.Header;
 import com.study.test.model.network.request.UserApiRequest;
 import com.study.test.model.network.response.UserApiResponse;
@@ -20,9 +23,20 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     // 3. 생성된 데이터 -> UserApiResponse return
 
     @Override
-    public Header<UserApiResponse> create(UserApiRequest request) {
-        // TODO Auto-generated method stub
-        return null;
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        // 1. request data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. User 생성
+        User user = User.builder().account(userApiRequest.getAccount()).password(userApiRequest.getPassword())
+                .status("REGISTERED").phoneNumber(userApiRequest.getPhoneNumber()).email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now()).build();
+
+        User newUser = UserRepository.save(user);
+
+        // 3. 생성된 데이터 => userApiResponse return
+        return response(newUser);
     }
 
     @Override
@@ -32,7 +46,7 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     }
 
     @Override
-    public Header<UserApiResponse> update(UserApiRequest request) {
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -41,6 +55,18 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     public Header delete(Long id) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private Header<UserApiResponse> response(User user) {
+        // user -> userApiResponse
+
+        UserApiResponse userApiResponse = UserApiResponse.builder().id(user.getId()).account(user.getAccount())
+                .password(user.getPassword()).email(user.getEmail()).phoneNumber(user.getPhoneNumber())
+                .registeredAt(user.getRegisteredAt()).unregisteredAt(user.getUnregisteredAt()).build();
+
+        // Header + data return
+
+        return Header.OK(userApiResponse);
     }
 
 }
