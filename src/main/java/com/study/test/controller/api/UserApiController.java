0f051c243a55ac60752f4b3e6fdf2c5046c2f149
hws522 <sraphic@kakao.com>
2020-12-02 18:44:05 +0900

    CRUD interface

--- src/main/java/com/study/test/controller/api/UserApiController.java
+++ src/main/java/com/study/test/controller/api/UserApiController.java
@@ -2,36 +2,39 @@ package com.study.test.controller.api;
 
 import com.study.test.ifs.CrudInterface;
 import com.study.test.model.network.Header;
+import com.study.test.model.network.request.UserApiRequest;
+import com.study.test.model.network.response.UserApiResponse;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
     @Override
     @PostMapping("") // /api/user
-    public Header create() {
+    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     @GetMapping("{id}") // /api/user/{id}
-    public Header read(Long id) {
+    public Header<UserApiResponse> read(Long id) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     @PutMapping("") // /api/user
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody UserApiRequest userApiRequest) {
         // TODO Auto-generated method stub
         return null;
     }
--- src/main/java/com/study/test/ifs/CrudInterface.java
+++ src/main/java/com/study/test/ifs/CrudInterface.java
@@ -2,13 +2,13 @@ package com.study.test.ifs;
 
 import com.study.test.model.network.Header;
 
-public interface CrudInterface {
+public interface CrudInterface<Req, Res> {
 
-    Header create(); // todo request object 추가
+    Header<Res> create(Req request); // todo request object 추가
 
-    Header read(Long id);
+    Header<Res> read(Long id);
 
-    Header update();
+    Header<Res> update(Req request);
 
     Header delete(Long id);
 }
