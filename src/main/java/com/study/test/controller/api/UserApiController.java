24a1433cde16360fe7b0f299627b424b5a04127f
hws522 <sraphic@kakao.com>
2020-12-02 22:42:37 +0900

    user-create add

--- src/main/java/com/study/test/controller/api/UserApiController.java
+++ src/main/java/com/study/test/controller/api/UserApiController.java
@@ -4,7 +4,9 @@ import com.study.test.ifs.CrudInterface;
 import com.study.test.model.network.Header;
 import com.study.test.model.network.request.UserApiRequest;
 import com.study.test.model.network.response.UserApiResponse;
+import com.study.test.service.UserApiLogicService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,15 +16,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
+
     @Override
     @PostMapping("") // /api/user
-    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
-        // TODO Auto-generated method stub
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}", request);
+        return userApiLogicService.create(request);
     }
 
     @Override
@@ -34,7 +42,7 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
 
     @Override
     @PutMapping("") // /api/user
-    public Header<UserApiResponse> update(@RequestBody UserApiRequest userApiRequest) {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
         // TODO Auto-generated method stub
         return null;
     }
