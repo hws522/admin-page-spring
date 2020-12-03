baff3bcef318bbab9533c98742f67395b9dc5851
hws522 <sraphic@kakao.com>
2020-12-04 01:21:40 +0900

    OrderGroup API create add

---
+++ src/main/java/com/study/test/controller/api/OrderGroupApiController.java
@@ -0,0 +1,50 @@
+package com.study.test.controller.api;
+
+import com.study.test.ifs.CrudInterface;
+import com.study.test.model.network.Header;
+import com.study.test.model.network.request.OrderGroupApiRequest;
+import com.study.test.model.network.response.OrderGroupApiResponse;
+import com.study.test.service.OrderGroupApiLogicService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orderGroup")
+public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    private OrderGroupApiLogicService orderGroupApiLogicService;
+
+    @Override
+    @PostMapping("")
+    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
+        return orderGroupApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("")
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/study/test/service/OrderGroupApiLogicService.java
@@ -0,0 +1,66 @@
+package com.study.test.service;
+
+import java.time.LocalDateTime;
+
+import com.study.test.ifs.CrudInterface;
+import com.study.test.model.entity.OrderGroup;
+import com.study.test.model.network.Header;
+import com.study.test.model.network.request.OrderGroupApiRequest;
+import com.study.test.model.network.response.OrderGroupApiResponse;
+import com.study.test.repository.OrderGroupRepository;
+import com.study.test.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
+
+        OrderGroupApiRequest body = request.getData();
+
+        OrderGroup orderGroup = OrderGroup.builder().status(body.getStatus()).orderType(body.getOrderType())
+                .revAddress(body.getRevAddress()).revName(body.getRevName()).paymentType(body.getPaymentType())
+                .totalPrice(body.getTotalPrice()).totalQuantity(body.getTotalQuantity()).orderAt(LocalDateTime.now())
+                .user(userRepository.getOne(body.getUserId())).build();
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+
+        return response(newOrderGroup);
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup) {
+
+        OrderGroupApiResponse body = OrderGroupApiResponse.builder().id(orderGroup.getId())
+                .status(orderGroup.getStatus()).orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress()).revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getPaymentType()).totalPrice(orderGroup.getTotalPrice())
+                .totalQuantity(orderGroup.getTotalQuantity()).orderAt(orderGroup.getOrderAt())
+                .arrivalDate(orderGroup.getArrivalDate()).userId(orderGroup.getUser().getId()).build();
+        return Header.OK(body);
+    }
+
+}
