8617da96ee93dbef3158e1aeede0b6c104ea062f
hws522 <sraphic@kakao.com>
2020-12-04 00:03:10 +0900

    OrderGroup Api req, res add

---
+++ src/main/java/com/study/test/model/network/response/OrderGroupApiResponse.java
@@ -0,0 +1,39 @@
+package com.study.test.model.network.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderGroupApiResponse {
+
+    private Long id;
+
+    private String status;
+
+    private String orderType;
+
+    private String revAddress;
+
+    private String revName;
+
+    private String paymentType;
+
+    private BigDecimal totalPrice;
+
+    private Integer totalQuantity;
+
+    private LocalDateTime orderAt;
+
+    private LocalDateTime arrivalDate;
+
+    private Long userId;
+
+}
