37a2f4b0cc2f91117068e46f0c6ebf74a737f6d5
hws522 <sraphic@kakao.com>
2020-12-04 09:18:13 +0900

    front view add

---
+++ src/main/java/com/study/test/service/AdminMenuService.java
@@ -0,0 +1,18 @@
+package com.study.test.service;
+
+import com.study.test.model.front.AdminMenu;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class AdminMenuService {
+
+    public List<AdminMenu> getAdminMenu() {
+
+        return Arrays.asList(AdminMenu.builder().title("고객 관리").url("/pages/user").code("user").build());
+
+    }
+
+}
